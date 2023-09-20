d9489ede2a655b22c7de9c320ac09349ef552a87
Sanket <sanketbhat882002@gmail.com>
2023-09-20 09:22:05 +0530

    made the css better

--- app.py
+++ app.py
@@ -18,30 +18,33 @@ def hello_world():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get user input from the form
-    area = request.form['area']
-    oxygen = float(request.form['oxygen'])
-    temperature = float(request.form['temperature'])
-    humidity = float(request.form['humidity'])
-    wind_speed = float(request.form['wind_speed'])
-    vegetation_density = float(request.form['vegetation_density'])
-    proximity_to_water = request.form['proximity_to_water']
-    soil_type = request.form['soil_type']
+    try:
+        # Get user input from the form
+        area = request.form['area']
+        oxygen = float(request.form['oxygen'])
+        temperature = float(request.form['temperature'])
+        humidity = float(request.form['humidity'])
+        wind_speed = float(request.form['wind_speed'])
+        vegetation_density = float(request.form['vegetation_density'])
+        proximity_to_water = request.form['proximity_to_water']
+        soil_type = request.form['soil_type']
 
-    # Create a new DataFrame with the user input
-    user_data = pd.DataFrame([[area, oxygen, temperature, humidity, wind_speed, vegetation_density, proximity_to_water, soil_type]],
-                             columns=["Area", "Oxygen", "Temperature", "Humidity", "Wind Speed", "Vegetation Density", "Proximity to Water", "Soil Type"])
+        # Create a new DataFrame with the user input
+        user_data = pd.DataFrame([[area, oxygen, temperature, humidity, wind_speed, vegetation_density, proximity_to_water, soil_type]],
+                                columns=["Area", "Oxygen", "Temperature", "Humidity", "Wind Speed", "Vegetation Density", "Proximity to Water", "Soil Type"])
 
-    # Make a prediction
-    prediction = model.predict(user_data)
-    probability = model.predict_proba(user_data)[:, 1]
+        # Make a prediction
+        prediction = model.predict(user_data)
+        probability = model.predict_proba(user_data)[:, 1]
 
-    if prediction[0] == 1:
-        prediction_text = 'Your Forest is in Danger.'
-    else:
-        prediction_text = 'Your Forest is Safe.'
+        prediction_text = 'Your Forest is in Danger.' if prediction[0] == 1 else 'Your Forest is Safe.'
+        prediction_class = 'danger' if prediction[0] == 1 else 'safe'
 
-    return render_template('forest_fire.html', prediction=prediction_text, probability=f'{probability[0]:.2f}')
+        return render_template('forest_fire.html', prediction=prediction_text, prediction_class=prediction_class, probability=f'{probability[0]:.2f}')
+    except Exception as e:
+        # Handle errors and display an error message
+        error_message = f"An error occurred: {str(e)}"
+        return render_template('forest_fire.html', error=error_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
